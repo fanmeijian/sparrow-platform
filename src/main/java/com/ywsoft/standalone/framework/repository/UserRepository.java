e16df0223e445d38d4e3dbe723051c30172b6392
fanmeijian <fanmeijian@aliyun.com>
2020-12-14 17:44:30 +0800

    增加用户名模糊搜索功能

--- src/main/java/com/ywsoft/standalone/framework/repository/UserRepository.java
+++ src/main/java/com/ywsoft/standalone/framework/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.ywsoft.standalone.framework.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,4 +17,12 @@ public interface UserRepository extends JpaRepository<SwdUser, String> {
 
 	@Query("SELECT s.swdSysroles FROM SwdUser s WHERE s.username = ?1")
 	List<SwdSysrole> userSysroles(String username);
+	
+	
+//	@Query("select u from User u where u.lastname like %:#{[0]}% and u.lastname like %:lastname%")
+//	List<User> findByLastnameWithSpelExpression(@Param("lastname") String lastname);
+	
+	
+	Page<SwdUser> findByUsernameContaining(String username,Pageable p);
+
 }
