a693adc03efe762da1357c48fc815c8e94caca0a
fanmeijian <fanmeijian@aliyun.com>
2020-12-19 13:05:15 +0800

    组织单元，级别，岗位增加按公司id查找

--- src/main/java/com/ywsoft/standalone/framework/repository/LevelRepository.java
+++ src/main/java/com/ywsoft/standalone/framework/repository/LevelRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ywsoft.standalone.framework.entity.SwdLevel;
-import com.ywsoft.standalone.framework.entity.SwdOrganization;
 
 public interface LevelRepository extends JpaRepository<SwdLevel, String> {
-	Page<SwdLevel> findBySwdOrganization(SwdOrganization organization,Pageable p);
+	Page<SwdLevel> findBySwdOrganizationId(String organizationId,Pageable p);
 }
--- src/main/java/com/ywsoft/standalone/framework/repository/UnitRepository.java
+++ src/main/java/com/ywsoft/standalone/framework/repository/UnitRepository.java
@@ -2,6 +2,8 @@ package com.ywsoft.standalone.framework.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,4 +14,7 @@ public interface UnitRepository extends JpaRepository<SwdUnit, String> {
 	
 	@Query("SELECT new com.ywsoft.standalone.framework.entity.ext.UnitExt(s) FROM SwdUnit s WHERE (?1 IS null OR s.parentId = ?1) AND s.swdOrganization.id = ?2")
 	List<UnitExt> getByParentAndOrganization(String parentId,String organizationId);
+	
+	Page<SwdUnit> findBySwdOrganizationId(String organizationId, Pageable p);
+	
 }
